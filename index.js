1b477ad7f585016ee688f4fab0f8739311cfcacf
Jack Kitto <jack.kitto@gmail.com>
2022-10-30 17:33:08 +1100

    changes

--- index.js
+++ index.js
@@ -1,19 +1,19 @@
-// // Add this to the VERY top of the first file loaded in your app
-// var apm = require('elastic-apm-node').start({
+// Add this to the VERY top of the first file loaded in your app
+var apm = require('elastic-apm-node').start({
 
-// // Override the service name from package.json
-// // Allowed characters: a-z, A-Z, 0-9, -, _, and space
-// serviceName: '',
+// Override the service name from package.json
+// Allowed characters: a-z, A-Z, 0-9, -, _, and space
+serviceName: '',
 
-// // Use if APM Server requires a secret token
-// secretToken: 'vIElUgVeOk9vBHvxFq',
+// Use if APM Server requires a secret token
+secretToken: 'vIElUgVeOk9vBHvxFq',
 
-// // Set the custom APM Server URL (default: http://localhost:8200)
-// serverUrl: 'https://821af7082a0548e3acdf8828ce11a771.apm.us-central1.gcp.cloud.es.io:443',
+// Set the custom APM Server URL (default: http://localhost:8200)
+serverUrl: 'https://821af7082a0548e3acdf8828ce11a771.apm.us-central1.gcp.cloud.es.io:443',
 
-// // Set the service environment
-// environment: 'production'
-// })
+// Set the service environment
+environment: 'production'
+})
 
 var express = require('express')
 var morgan = require('morgan')
